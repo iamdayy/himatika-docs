d26a924e57f60def5bbe02fb52565611c04b7bfc
iamdayy <108299143+iamdayy@users.noreply.github.com>
2026-03-13 18:41:26 +0700

    feat: add new documentation pages for certificate management, projects, gallery, and signatures, and integrate the `nuxt-llms` module for content configuration.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/content"],
+  modules: ["@nuxt/content","nuxt-llms"],
   site: {
     name: 'Dokumentasi Resmi Situs Resmi HIMATIKA',
     description: 'Himpunan Mahasiswa Informatika'
-  }
+  },
+  llms: {
+    domain: 'https://docs.himatika-itsnupekalongan.com',
+    title: 'Dokumentasi Resmi Situs Resmi HIMATIKA',
+    description: 'Himpunan Mahasiswa Informatika',
+    sections: [
+      {
+        title: 'Panduan Admin',
+        description: 'Panduan lengkap untuk admin',
+        links: [
+          {
+            title: 'Kelola Sertifikat',
+            description: 'Panduan lengkap mendesain, mengonfigurasi, dan menerbitkan sertifikat digital untuk peserta dan panitia agenda.',
+            href: '/4.panduan-admin/9.kelola-sertifikat',
+          },
+          {
+            title: 'Kelola Agenda',
+            description: 'Panduan lengkap untuk mengelola agenda',
+            href: '/4.panduan-admin/1.kelola-agenda',
+          },
+        ],
+      },
+    ],
+  },
 })
